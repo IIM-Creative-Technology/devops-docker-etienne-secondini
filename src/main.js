efa4fd2894e3ec68e29b960ce2e4e73297ead9e8
etsec-cloud <etienne.secondini@hotmail.fr>
2021-11-14 21:27:58 +0100

    add secué

--- src/main.js
+++ src/main.js
@@ -2,7 +2,7 @@ import Vue from "vue";
 import App from "./App.vue";
 import axios from "axios";
 import vuetify from "./plugins/vuetify";
-import router from './router'
+import router from "./router";
 Vue.prototype.$http = axios;
 
 Vue.config.productionTip = false;
@@ -10,5 +10,5 @@ Vue.config.productionTip = false;
 new Vue({
   vuetify,
   router,
-  render: h => h(App)
+  render: h => h(App),
 }).$mount("#app");
