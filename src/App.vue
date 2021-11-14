914a84d7356ee4c25732c82b9ab3a988aed0d779
etsec-cloud <etienne.secondini@hotmail.fr>
2021-11-14 15:01:50 +0100

    first commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <PokemonList />
+  </div>
+</template>
+
+<script>
+import PokemonList from "./views/PokemonList.vue";
+
+export default {
+  name: "App",
+  components: {
+    PokemonList,
+  },
+};
+</script>
+
+<style>
+#app {
+  font-family: Avenir, Helvetica, Arial, sans-serif;
+  -webkit-font-smoothing: antialiased;
+  -moz-osx-font-smoothing: grayscale;
+  color: #2c3e50;
+  margin-top: 60px;
+}
+</style>
